d6dd803f08c3bf3ef5e724bafd29adb85568141d
Islomjon0221 <islomjon20110221@gmail.com>
2023-12-06 19:20:51 +0500

    Oxirigisi Ishlanmadi

---
+++ main.js
@@ -0,0 +1,210 @@
+// 1-misol
+
+// let a1 = +prompt("Son kiriting");
+// let sum1 = 0;
+// let i = 1
+
+// while (i < a1) {
+//     if (i % 2 == 0){
+//         sum1 += i;
+//     }
+//     i++
+// }
+
+// console.log(sum1);
+
+// 2-misol
+
+// let a2 = +prompt("Son kiriting");
+// let sum2 = 1;
+// let i = 1
+
+// while (i < a2 ) {
+//     if (i % 3 == 0){
+//         sum2 *= i;
+//     }
+//     i++;
+// }
+
+// console.log(sum2);
+
+// 3-misol
+
+// let a3 = +prompt("a sonni kiriting");
+// let b3 = +prompt("b sonni kiriting");
+// let sum3 = 0;
+// let i = a3
+
+// while ( i < b3) {
+//     if (i % 3 == 0 && i % 7 == 0){
+//         sum3++;
+//     }
+//     i++
+// }
+
+// console.log(sum3);
+
+// 4-misol
+
+// let a4 = +prompt("Son kiriting");
+// let sum4 = 0;
+// let res4;
+// let i = 1
+
+// while ( i <= a4){
+//     if (a4 % i == 0) {
+//         sum4++;
+//     }
+
+//     if (sum4 == 2){
+//         res4 = "Tub son"
+//     } else {res4 = "Tub son emas"}
+//     i++
+// }
+
+// console.log(res4);
+
+// 5-misol
+
+// let a5 = +prompt("Sonni kiriting");
+// let sum5 = 0;
+// let i = 1
+// while (i <= a5) {
+//         sum5 += i;
+//         i++
+// }
+
+// console.log(sum5);
+
+
+// 6-misol
+
+// let a6 = +prompt(`Uch xonali son kiriting`);
+// let f = a6 % 10 
+// let o = Math.trunc((a6 % 100) / 10) 
+// let h = Math.trunc(a6 / 100);
+// let res;
+// let x;
+
+// switch (f) {
+//     case 1:
+//         res = 'bir';
+//         break;
+        
+//         case 2:
+// res = 'ikki';
+// break;
+
+// case 3:
+// res = 'uch';
+// break;
+
+// case 4:
+// res = "to'rt";
+// break;
+
+// case 5:
+// res = 'besh';
+// break;
+
+// case 6:
+//     res = 'olti';
+//     break;
+    
+//     case 7:
+//         res = 'yetti';
+//         break;
+        
+// case 8:
+// res = 'sakkiz';
+// break;
+
+// case 9:
+// res = "to'qqiz";
+// break;
+// default:
+// res = 'Bunday operatsiya mavjud emas';
+// break;
+// }
+// switch (o) {
+//     case 1:
+//         res = `o'n ${res}`;
+//         break;
+
+//         case 2:
+//             res = `yigirma ${res}`;
+//             break;
+            
+//             case 3:
+//                 res = `o'ttiz ${res}`;
+//         break;
+
+//     case 4:
+//         res = `qiriq ${res}`;
+//         break;
+        
+//         case 5:
+//             res = `ellik ${res}`;
+//             break;
+            
+//             case 6:
+//         res = `oltmish ${res}`;
+//         break;
+        
+//         case 7:
+//             res = `yetmish ${res}`;
+//             break;
+            
+//             case 8:
+//                 res = `sakson ${res}`;
+//                 break;
+                
+//                 case 9:
+//                     res = `to'qson ${res}`;
+//                     break;
+//                     default:
+//                         res = 'Bunday operatsiya mavjud emas';
+//                         break;
+// }                    
+// switch (h) {
+//                 case 1:
+//                     res = `Bir yuz ${res}`;
+//                     break;
+            
+//                 case 2:
+//                     res = `Ikki yuz ${res}`;
+//                     break;
+            
+//                 case 3:
+//                     res = `Uch yuz ${res}`;
+//                     break;
+            
+//                 case 4:
+//                     res = `To'rt yuz ${res}`;
+//                     break;
+            
+//                 case 5:
+//                     res = `Besh yuz ${res}`;
+//                     break;
+            
+//                 case 6:
+//                     res = `Olti yuz ${res}`;
+//                     break;
+            
+//                 case 7:
+//                     res = `Yetti yuz ${res}`;
+//                     break;
+            
+//                 case 8:
+//                     res = `Sakkiz yuz ${res}`;
+//                     break;
+            
+//                 case 9:
+//                     res = `To'qqiz yuz ${res}`;
+//                     break;
+//                 default:
+//                     res = 'Bunday operatsiya mavjud emas';
+//                     break;
+// }
+
+// console.log(res);
